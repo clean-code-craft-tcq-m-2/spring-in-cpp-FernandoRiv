74f7c3802b2046518e7d4cc44499321b67615594
Fernando Riveros <fer96nando@gmail.com>
2022-09-12 22:29:42 -0500

    Removed Alerter files, added content to stats

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,7 @@
 #include "stats.h"
 
+// ==== Statistics ====
+
 Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& values) {
     //Implement statistics here
     Stats retVal;
@@ -47,4 +49,19 @@ void Statistics::NanReturn(Stats& stats){
     stats.average = 1;
     stats.min = 1;
     stats.max = 1;
+}
+
+// ==== Alerts ====
+
+void Alerts::StatsAlerter::checkAndAlert(const std::vector<float>& data){
+    if(Statistics::GetMax(data) > mThreshold){
+        for(IAlerter *alerter: mAlerters){
+            alerter->SetAlert(true);
+        }
+    }
+    else{
+        for(IAlerter *alerter: mAlerters){
+            alerter->SetAlert(false);
+        }
+    }
 }
--- stats.h
+++ stats.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 
+// ==== Statistics ====
 
 namespace Statistics {
     class Stats{
@@ -17,6 +18,51 @@ namespace Statistics {
     float GetMin(const std::vector<float>& values);
     float GetMax(const std::vector<float>& values);
     void  NanReturn(Stats& stats);
+}
+
+// ==== Alerts ====
+
+namespace Alerts{
+    class IAlerter
+    {
+        public:
+            virtual ~IAlerter();
+            virtual void SetAlert(bool) = 0;
+    };
+
+    class EmailAlert: public IAlerter
+    {
+        public:
+            bool emailSent = false;
+
+            virtual void SetAlert(bool setAlert){
+                emailSent = setAlert;
+            }
+    };
+
+    class LEDAlert: public IAlerter
+    {
+        public:
+            bool ledGlows = false;
+
+            virtual void SetAlert(bool setAlert){
+                ledGlows = setAlert;
+            }
+    };
+
+    class StatsAlerter
+    {
+        public:
+            StatsAlerter(const float threshold, std::vector<Alerts::IAlerter*> alerters):
+                mThreshold(threshold),
+                mAlerters(alerters) {}
+
+            void checkAndAlert(const std::vector<float>& data);
+
+        private:
+            float mThreshold;
+            std::vector<Alerts::IAlerter*> mAlerters;
+    };
 }
 
 #endif // _STATS_H
